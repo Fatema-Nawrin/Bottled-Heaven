076698a784150134ba6a6775cda1df322de7f46b
Fatema-Nawrin <nawrin4000@gmail.com>
2022-03-26 12:28:22 +0600

    data file added

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const Card = () => {
+const Card = (props) => {
+    const { name, price, img } = props.product;
     return (
         <div>
-
+            <img src={img} alt="" />
+            <div className="product-info">
+                <h4>{name}</h4>
+                <p>Price: ${price}</p>
+            </div>
         </div>
     );
 };
