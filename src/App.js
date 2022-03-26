cb61a5a66099ca4d8de8d8c0cb4537fd0706029d
Fatema-Nawrin <nawrin4000@gmail.com>
2022-03-26 23:20:00 +0600

    last added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ function App() {
     <div className="App">
       <Header></Header>
       <Shop></Shop>
+      <div className='QA-section'>
+        <h1>React Questions and Answers</h1>
+        <h2>How does React work?</h2>
+        <p>React is a javascript library. React has virtual DOM which is only a represntation of  UI and synced with real DOM. Whenever there is a change in state, a new virtual DOM is created and compared with the previous one and find the differences. After that, DOM only update that elements where changes happened. That's why react is efficient.</p>
+        <h2>Props Vs State</h2>
+        <p>Props are read only. Props are passed into components and as attributes in html. Props can not be modified. It is immutable. On the other hand, state is mutable. State is used when there is a need of updating information. Changes in state is asynchronous.</p>
+        <h2>How does useState work?</h2>
+        <p>useState is a hook function. This function returns a pair of values; one is a stateful value and another is a function to update it after passing an initial state. And the state can be a number, string or object whatever is needed.</p>
+      </div>
     </div>
   );
 }
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,6 +4,8 @@ import './Cart.css'
 const Cart = (props) => {
     const { cart } = props;
     const { reset, chooseOne, item } = props;
+    console.log(item);
+    console.log(item.length);
 
     return (
         <div className='cart'>
@@ -19,9 +21,14 @@ const Cart = (props) => {
             <button className='cart-btn' onClick={chooseOne}>Choose One For Me</button>
             <br />
             <button className='cart-btn' onClick={reset}>Reset</button>
-            <div key={item.id} className='selected-item'>
+            <div key={item.id} className={item.length === 0 ? 'hidden-item' : 'visible-item'}>
+                <h3>Chosen One:</h3>
+                <img src={item.img} alt="" />
                 <h4>{item.name}</h4>
-            </div >
+            </div>
+
+
+
         </div >
     );
 };
